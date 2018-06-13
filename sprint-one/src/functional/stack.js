25b87209acef0a8698c5f9c484141323750de477
Calin Lewis <calin.lewis@gmail.com>
2018-06-13 13:04:49 -0700

    Working: Stack (Adding 2 Items Completed)
    Test to add two items is completed.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,9 +6,29 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    // Check to see if anything exists within storage.
+    // If so, apply a key of number 0 and a value = value:
+    if(Object.keys(storage).length === 0){
+      storage[0] = value;
+    } else {
+      // If storage is not empty, find the value of the last key:
+      var lastKey = Object.keys(storage)[storage.length - 1];
+
+      // Identify the next available number to be used as a key:
+      var nextAvailable = parseInt(lastKey) + 1;
+
+      // Create a new key\value pair within storage that is 
+      // the combination of 'nextAvailable' and the passed in value: 
+      storage[nextAvailable] = value;
+
+      // Return the new size of the storage object:
+      return Object.keys(storage);
+    }
+
   };
 
   someInstance.pop = function() {
+
   };
 
   someInstance.size = function() {
