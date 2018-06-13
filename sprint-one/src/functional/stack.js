9524c7a0ecc7ca8fda16c54d4d42530315d19a5c
Calin Lewis <calin.lewis@gmail.com>
2018-06-13 12:53:48 -0700

    Working: Stack (Size Test Completed)
    Passing size tests.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -12,6 +12,14 @@ var Stack = function() {
   };
 
   someInstance.size = function() {
+    // If the storage is empty, return 0:
+    if(Object.keys(storage).length === 0){
+      return 0;
+    } else {
+    // Otherwise, return the length of the storage object by counting the 
+    // number of keys it contains:
+      return Object.keys(storage).length;
+    } 
   };
 
   return someInstance;
