3b85584cd3579d6f7617f035eec8d79355bac639
Calin Lewis <calin.lewis@gmail.com>
2018-06-14 00:04:30 -0700

    Completed: Stack.js (Pseudoclassical)
    All tests passing

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,37 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.elementCount = Object.keys(this).length; 
+  this.nextAvailable = this.elementCount + 1;
 };
 
 
+Stack.prototype.pop = function(){
+  if(this.elementCount === 0){
+    return 0;
+  } else {
+    var removed = this[this.elementCount - 1];
+    this.elementCount--;
+    delete this[this.elementCount];
+    return removed;
+  }
+};
+
+Stack.prototype.push = function(value){
+  if(this.elementCount === 0){
+    this[0] = value;
+    this.elementCount++; 
+    return this.elementCount;
+  } else {
+    this[this.nextAvailable] = value;
+    this.elementCount++;
+    this.nextAvailable++;
+    return this.elementCount;
+  }
+};
+
+Stack.prototype.size = function(){
+  if(this.elementCount === 0){
+    return 0;
+  } else {
+    return this.elementCount;
+  }
+};
