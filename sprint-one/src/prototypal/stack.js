c2d2e9dc8bb5fa243779dfb5d46c6041a0a080a8
Calin Lewis <calin.lewis@gmail.com>
2018-06-13 23:49:11 -0700

    Completed: Stack.js (Prototypal Pattern)
    All tests passing

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,46 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+
+  var newStack = Object.create(stackMethods);
+  newStack.elementCount = Object.keys(newStack).length;
+  newStack.nextAvailable = newStack.elementCount + 1;
+
+  return newStack;
 };
 
-var stackMethods = {};
+var stackMethods = {
 
+  'pop': function() {
+    if(this.elementCount === 0){
+      return 0;
+    } else {
+      var removed = this[this.elementCount - 1];
+      this.elementCount--;
+      delete this[this.elementCount];
+      return removed;
+    }
+
+  },
+
+  'push': function(value){
+    // If the object is empty, assign value to key of 0:
+    if(this.elementCount === 0){
+      this[0] = value;
+      this.elementCount++; 
+      return this.elementCount;
+    } else {
+      this[this.nextAvailable] = value;
+      this.elementCount++;
+      this.nextAvailable++;
+      return this.elementCount;
+    }
+  },
+
+  'size': function(){
+    if(this.elementCount === 0){
+      return 0;
+    } else {
+      return this.elementCount;
+    }
+  }
+};
 
