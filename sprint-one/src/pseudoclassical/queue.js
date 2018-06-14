a7de64fac7157c4a14b058cd2fae089aed0c68a8
Calin Lewis <calin.lewis@gmail.com>
2018-06-14 00:11:38 -0700

    Completed: Queue.js (Pseudoclassical)
    All tests passing

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,42 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.keys =  Object.keys(this);
+  this.elementCount = Object.keys(this).length - 1;
+  this.nextAvailable = this.elementCount + 1;
 };
 
+Queue.prototype.enqueue = function(value){
+  if(this.elementCount === 0){
+    this[0] = value;
+    this.nextAvailable = 1;
+    this.elementCount = 1;
+    return this.elementCount;
+  } else {
+    this[this.nextAvailable] = value;
+    this.nextAvailable++;
+    this.elementCount++;
+    return this.elementCount;
+  }
+}
 
+Queue.prototype.dequeue = function(){
+  if(this.elementCount === 0){
+    return 0;
+  }
+  var elementsForKeyShift = Object.keys(this).slice(0, this.elementCount);
+  var itemRemoved = this[elementsForKeyShift[0]];
+  this.elementCount--;
+  delete this[elementsForKeyShift[0]];
+  elementsForKeyShift.forEach(function(key){
+    parseInt(key);
+    key = key - 1;
+  });
+  return itemRemoved;
+}
+
+Queue.prototype.size = function(){
+  if(this.elementCount === 0){
+    return 0;
+  } else {
+    return this.elementCount;
+  }
+}
